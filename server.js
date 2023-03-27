f3fde79bd46b3cd4f17bdef152a55439c4fb2470
Ebba Randgaard <ebbabr17@uia.no>
2023-03-28 01:01:09 +0200

    comment out JS thats not in use

--- server.js
+++ server.js
@@ -6,27 +6,27 @@ const app = express();
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'frontend')));
 
-// Serve the index page for all other routes
-app.get('*', (req, res) => {
-  const indexFilePath = path.join(__dirname, 'frontend', 'index.html');
-  res.sendFile(indexFilePath, err => {
-    if (err) {
-      console.error(`Error sending file: ${indexFilePath}`, err);
-      res.status(err.status).end();
-    }
-  });
-});
+// // Serve the index page for all other routes
+// app.get('*', (req, res) => {
+//   const indexFilePath = path.join(__dirname, 'frontend', 'index.html');
+//   res.sendFile(indexFilePath, err => {
+//     if (err) {
+//       console.error(`Error sending file: ${indexFilePath}`, err);
+//       res.status(err.status).end();
+//     }
+//   });
+// });
 
 
-// Serve event.js for /event.js route
-app.get('/event.js', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'event.js'));
-});
+// // Serve event.js for /event.js route
+// app.get('/event.js', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'frontend', 'event.js'));
+// });
 
-// Serve form.js for /form route
-app.get('/form.js', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'form.js'));
-});
+// // Serve form.js for /form route
+// app.get('/form.js', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'frontend', 'form.js'));
+// });
 
 // Start the server
 const port = process.env.PORT || 8080;
